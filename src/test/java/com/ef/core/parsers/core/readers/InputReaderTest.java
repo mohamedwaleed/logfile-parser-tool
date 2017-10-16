093fd68a8df46bedaeb53b154cfd1861b7025398
Mohamed Waleed <mwaleed.ext@orange.com>
2017-10-16 14:39:27 +0200

    add some tests

---
+++ src/test/java/com/ef/core/parsers/core/readers/InputReaderTest.java
@@ -0,0 +1,7 @@
+package com.ef.core.parsers.core.readers;
+
+/**
+ * Created by mohamed_waleed on 16/10/17.
+ */
+public class InputReaderTest {
+}
---
+++ src/test/java/com/ef/core/parsers/repositories/LogArchiveRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.ef.core.parsers.repositories;
+
+/**
+ * Created by mohamed_waleed on 16/10/17.
+ */
+public class LogArchiveRepositoryTest {
+}
